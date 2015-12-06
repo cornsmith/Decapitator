fe63eaeda124635325ed486934eb1c10478094a0
Stafford Williams <stafford.williams@gmail.com>
2015-12-06 22:16:30 +1100

    added argument check

--- decapitate.py
+++ decapitate.py
@@ -3,6 +3,10 @@ import cv2
 import sys
 import numpy as np
 
+if len(sys.argv) != 3:
+    print("Usage: python decapitat.py before.png after.png")
+    exit()
+
 # Parameters
 imageInPath = sys.argv[1]
 imageOutPath = sys.argv[2]
